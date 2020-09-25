140057d1a6c8008cee9e765b920717d1bd82411e
VickyRus95 <victoriarustraducciones@gmail.com>
2020-09-25 15:35:43 -0300

    Exc

---
+++ EXC JS/loops.js
@@ -0,0 +1,13 @@
+//recorrer=barrer
+//ciclo while = primero preguntamos y luego disparamos
+var i = 0;
+while (i < 10) {
+    document.write (i + "<br>");
+    i++;
+}
+document.write ("Salida: " + i + "<br>")
+//ciclo for = sabemos el numero de iteraciones
+
+//ciclo do while = primero disparamos y luego preguntamos
+
+//ciclo for in = para objetos y arreglos
