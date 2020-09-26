9646f6e599aa3968191644c93c4ad63864a9e355
VickyRus95 <victoriarustraducciones@gmail.com>
2020-09-26 13:53:46 -0300

    Exc011

---
+++ EDABIT/EJERC011/solucion011.js
@@ -0,0 +1,19 @@
+/*Create a function that returns true if an integer 
+is evenly divisible by 5, and false otherwise.
+Examples
+divisibleByFive(5) ➞ true
+divisibleByFive(-55) ➞ true
+divisibleByFive(37) ➞ false
+Notes
+Don't forget to return the result.*/
+function divisibleByFive (number){
+if (number % 2 == 0) {
+    console.log (true)
+} else {
+   console.log (false)
+}
+}
+divisibleByFive(5) 
+divisibleByFive(-50) 
+divisibleByFive(37)
+
