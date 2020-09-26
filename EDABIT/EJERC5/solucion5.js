b6d2dcefd53b3004b0f20ab616a148383f53cef6
VickyRus95 <victoriarustraducciones@gmail.com>
2020-09-26 11:20:45 -0300

    Exc5

---
+++ EDABIT/EJERC5/solucion5.js
@@ -0,0 +1,14 @@
+/*Create a function that takes length 
+and width and finds the perimeter of a rectangle.
+Examples
+findPerimeter(6, 7) ➞ 26
+findPerimeter(20, 10) ➞ 60
+findPerimeter(2, 9) ➞ 22
+Notes
+Don't forget to return the result.*/
+function findPerimeter (num1, num2){
+return (num1 + num2) * 2
+}
+console.log(findPerimeter(6, 7)) 
+console.log(findPerimeter(20, 10)) 
+console.log(findPerimeter(2, 9))
