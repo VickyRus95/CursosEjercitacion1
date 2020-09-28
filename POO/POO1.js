0d75f7a7f4a31708037d7e38419e3031f9f29b8d
VickyRus95 <victoriarustraducciones@gmail.com>
2020-09-28 12:35:00 -0300

    ExcPOO1

---
+++ POO/POO1.js
@@ -0,0 +1,17 @@
+const PeliculaUno = {
+    nombre: "Harry",
+    id: 01,
+    reproducir (){
+        return `Reproduciento pelicula... ${this.nombre}`;
+    }
+}
+console.log (PeliculaUno.reproducir())
+
+const PeliculaDos = {
+    nombre: "Araña",
+    id: 02,
+    reproducir (){
+        return `Reproduciento pelicula... ${this.nombre}`;
+    }
+}
+console.log (PeliculaDos.reproducir())
